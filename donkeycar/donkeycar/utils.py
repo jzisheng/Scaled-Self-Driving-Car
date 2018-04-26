b0120b17add2832f46961e7263e15f6203853c02
jzisheng <j.zisheng@gmail.com>
2018-04-25 21:09:31 -0400

    added average positioning and position notebooks

--- donkeycar/donkeycar/utils.py
+++ donkeycar/donkeycar/utils.py
@@ -404,3 +404,45 @@ def get_model_by_type(model_type, cfg):
         raise Exception("unknown model type: %s" % model_type)
 
     return kl
+
+
+def linear_bin(a):
+    a = a + 1
+    b = round(a / (2/14))
+    arr = np.zeros(15)
+    arr[int(b)] = 1
+    return arr
+
+
+def linear_unbin(arr):
+    b = np.argmax(arr)
+    a = b *(2/14) - 1
+    return a
+
+
+def bin_Y(Y):
+    d = []
+    for y in Y:
+        arr = np.zeros(15)
+        arr[linear_bin(y)] = 1
+        d.append(arr)
+    return np.array(d) 
+        
+def unbin_Y(Y):
+    d=[]
+    for y in Y:
+        v = linear_unbin(y)
+        d.append(v)
+    return np.array(d)
+
+def map_range(x, X_min, X_max, Y_min, Y_max):
+    ''' 
+    Linear mapping between two ranges of values 
+    '''
+    X_range = X_max - X_min
+    Y_range = Y_max - Y_min
+    XY_ratio = X_range/Y_range
+
+    y = ((x-X_min) / XY_ratio + Y_min) // 1
+
+    return int(y)
